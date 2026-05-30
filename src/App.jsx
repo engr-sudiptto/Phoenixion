d7b95ded4535218dd73e8105f401434a6c0e3061
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-30 20:58:52 +0600

    feat: implement dark and light mode toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
@@ -11,8 +11,21 @@ import { Toaster } from 'react-hot-toast';
 import Footer from './components/Footer';
 
 const App = () => {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem('theme') || 'light';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
-    <div>
+    <div
+      className={`${theme === 'dark' ? 'dark' : ''} min-h-screen dark:bg-black bg-no-repeat bg-cover`}
+      style={{
+        backgroundImage: theme === 'dark' ? 'none' : `url(${gradientBG})`,
+      }}
+    >
       <Toaster />
       <Navbar/>
       <Hero />
