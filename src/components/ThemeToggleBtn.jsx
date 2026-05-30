638706dac581a1974e8dd63069266a9e4b5fb2ce
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-30 21:09:43 +0600

    feat: add theme toggle button component

--- src/components/ThemeToggleBtn.jsx
+++ src/components/ThemeToggleBtn.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import light from '../assets/light.png';
+import dark from '../assets/night.png';
 
-const ThemeToggleBtn = () => {
+const ThemeToggleBtn = ({ theme, setTheme }) => {
   return (
-    <div>ThemeToggleBtn</div>
-  )
-}
+    <>
+      <button className="border w-7 h-7 rounded-full flex items-center justify-center cursor-pointer border-gray-900 dark:border-white lg:h-10 lg:w-10 dark:border">
+        {theme === 'dark' ? (
+          <img
+            className="w-4.5 h-4.5 object-cover invert lg:h-6 lg:w-6"
+            onClick={() => setTheme('light')}
+            src={light}
+          />
+        ) : (
+          <img
+            className="w-4 h-4 object-cover lg:h-5 lg:w-5"
+            onClick={() => setTheme('dark')}
+            src={dark}
+          />
+        )}
+      </button>
+    </>
+  );
+};
 
-export default ThemeToggleBtn
+export default ThemeToggleBtn;
