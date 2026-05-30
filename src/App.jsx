43afa2097540414ff240ebe8d87978c123b2523d
Sudipto Das <eng.sudiptodas@gmail.com>
2026-05-30 20:38:06 +0600

    feat: setup initial App.jsx layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import TrustedBy from './components/TrustedBy';
+import gradientBG from './assets/gradient_main_bg.jpg';
+import Services from './components/Services';
+import OurWork from './components/OurWork';
+import Teams from './components/Teams';
+import ContactUs from './components/ContactUs';
+import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Toaster />
+      <Navbar/>
+      <Hero />
+      <TrustedBy />
+      <Services />
+      <OurWork />
+      <Teams />
+      <ContactUs />
+      <Footer />
+    </div>
+  );
+};
 
-export default App
+export default App;
