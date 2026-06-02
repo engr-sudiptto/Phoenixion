649dcbbd9bc089abfa60c053fea7dde2b3894d66
Sudipto Das <eng.sudiptodas@gmail.com>
2026-06-02 11:18:11 +0600

    style: add smooth motion effect animation to contact form

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -41,8 +41,22 @@ const ContactUs = () => {
         <div className="lg:grid lg:grid-cols-2 lg:gap-5">
           {/* ==== name input ====== */}
           <div className="">
-            <p className="text-sm text-gray-500 mt-5 mb-3">Your Name</p>
-            <div className="flex items-center border px-2 rounded-md border-gray-400 h-10">
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              viewport={{ once: true }}
+              className="text-sm text-gray-500 mt-5 mb-3"
+            >
+              Your Name
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.7 }}
+              viewport={{ once: true }}
+              className="flex items-center border px-2 rounded-md border-gray-400 h-10"
+            >
               <img className="w-6 h-6 object-cover dark:invert" src={avter} />
               <input
                 name="name"
@@ -53,12 +67,26 @@ const ContactUs = () => {
                 placeholder="Enter your name"
                 required
               />
-            </div>
+            </motion.div>
           </div>
           {/* ======== email input =======  */}
-          <div>
-            <p className="text-sm text-gray-500 mt-5 mb-3">Email Id</p>
-            <div className="flex items-center border px-2 rounded-md border-gray-400 h-10 mb-5">
+          <div className="">
+            <motion.p
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+              viewport={{ once: true }}
+              className="text-sm text-gray-500 mt-5 mb-3"
+            >
+              Email Id
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.7 }}
+              viewport={{ once: true }}
+              className="flex items-center border px-2 rounded-md border-gray-400 h-10 mb-5"
+            >
               <img
                 className="w-6 h-4.2 object-cover dark:invert"
                 src={messageLogo}
@@ -72,19 +100,35 @@ const ContactUs = () => {
                 placeholder="Enter your email"
                 required
               />
-            </div>
+            </motion.div>
           </div>
         </div>
-        <p className="text-sm text-gray-500 mt-5 mb-3">Message</p>
-        <textarea
+        <motion.p
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.7 }}
+          viewport={{ once: true }}
+          className="text-sm text-gray-500 mt-5 mb-3"
+        >
+          Message
+        </motion.p>
+        <motion.textarea
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.9 }}
+          viewport={{ once: true }}
           name="message"
           value={formData.message}
           onChange={handleChange}
           className="w-full h-50 border border-gray-400 rounded-lg pt-2 pl-3 text-sm resize-none dark:text-gray-100 outline-0"
           placeholder="Enter your message"
           required
-        ></textarea>
-        <button
+        ></motion.textarea>
+        <motion.button
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 1 }}
+          viewport={{ once: true }}
           type="submit"
           onClick={submitBtn}
           className="w-full h-10 flex items-center justify-center mt-7 bg-linear-to-r from-blue-500 via-indigo-500 to-purple-500 rounded-md text-white gap-2 cursor-pointer hover:bg-linear-to-r hover:from-blue-600/80 hover:via-indigo-600/80 hover:to-purple-600/80"
@@ -99,12 +143,12 @@ const ContactUs = () => {
             className="size-4"
           >
             <path
-              fillRule="evenodd"
+              fill-rule="evenodd"
               d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z"
-              clipRule="evenodd"
+              clip-rule="evenodd"
             />
           </svg>
-        </button>
+        </motion.button>
       </form>
     </div>
   );
